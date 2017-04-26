b61ac71e5a6c43c1c47f9bb3e1f9f5c01edae8b6
robend1990 <robend1990@vp.pl>
2017-04-26 19:10:09 +0200

    deregistering unused filters and excluding autoconfiguration for components that are not used

--- src/main/java/com/rdrewniak/JustGifItApplication.java
+++ src/main/java/com/rdrewniak/JustGifItApplication.java
@@ -1,17 +1,26 @@
 package com.rdrewniak;
 
+import java.io.File;
+
+import javax.annotation.PostConstruct;
+
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jackson.JacksonAutoConfiguration;
+import org.springframework.boot.autoconfigure.jmx.JmxAutoConfiguration;
+import org.springframework.boot.autoconfigure.websocket.WebSocketAutoConfiguration;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
+import org.springframework.web.filter.HiddenHttpMethodFilter;
+import org.springframework.web.filter.HttpPutFormContentFilter;
+import org.springframework.web.filter.RequestContextFilter;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
-import javax.annotation.PostConstruct;
-import java.io.File;
-
-@SpringBootApplication
+@SpringBootApplication(exclude = {JacksonAutoConfiguration.class, JmxAutoConfiguration.class,
+		WebSocketAutoConfiguration.class})
 public class JustGifItApplication {
 
     @Value("${multipart.location}/gif/")
@@ -40,4 +49,42 @@ public class JustGifItApplication {
             }
         };
     }
+    
+    
+    /**
+     * Deregister filter which allows to use other http method that post and get 
+     * @param filter
+     * @return
+     */
+    @Bean
+    public FilterRegistrationBean deregisterHiddenHttpMethodFilter(HiddenHttpMethodFilter filter) {
+    	FilterRegistrationBean bean = new FilterRegistrationBean(filter);
+    	bean.setEnabled(false);
+    	return bean;
+    }
+    
+    /**
+     * We are not using PUT method. lets disable put filter
+     * @param filter
+     * @return
+     */
+    @Bean
+    public FilterRegistrationBean deregisterHttpPutFormContentFilter(HttpPutFormContentFilter filter) {
+    	FilterRegistrationBean bean = new FilterRegistrationBean(filter);
+    	bean.setEnabled(false);
+    	return bean;
+    }
+    
+    /**
+     * We are not using any request and session scoped beans. Lets disable filter
+     * @param filter
+     * @return
+     */
+    @Bean
+    public FilterRegistrationBean deregisterRequestContextFilter(RequestContextFilter filter) {
+    	FilterRegistrationBean bean = new FilterRegistrationBean(filter);
+    	bean.setEnabled(false);
+    	return bean;
+    }
+    
 }
